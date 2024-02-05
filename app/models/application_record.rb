ecd4f4809391f1f27c3bfbabfbb855e065672f45
kameron beamer <beamerkameron@gmail.com>
2024-02-05 05:35:42 +0000

    Added initial application_record description

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
+=begin
+Represents the abstract structure of the database.
+Superclass to organization, region, resource category, ticket, and user.
+=end
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end

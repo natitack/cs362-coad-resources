c17624cdfaa5e999d8c42ad5fc36fe3e69c87cb0
kameron beamer <beamerkameron@gmail.com>
2024-02-09 02:05:52 +0000

    Added User's uniqueness of email validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,9 +26,12 @@ RSpec.describe User, type: :model do
   it "User validates presence of email" do
     expect(@user1).to validate_presence_of(:email)
   end
-
   it "User validates length of email" do
     expect(@user1).to validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
   end
+  it "User validates email format" do
+    expect(@user1).to validate_uniqueness_of(:email).case_insensitive
+  end
+
 
 end

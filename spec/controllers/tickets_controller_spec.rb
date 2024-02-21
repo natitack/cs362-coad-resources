9bd9558226850c285e5088f5823a08dba5eb8ac8
natitack <114112085+natitack@users.noreply.github.com>
2024-02-21 23:50:21 +0000

    finish close ticket coverage from class

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -6,10 +6,38 @@ RSpec.describe TicketsController, type: :controller do
         context "as a logged out user" do
             it "redirects to dashboard" do
             ticket = create(:ticket)
-            patch :close, params: { id: ticket.id }
+            patch :close, params: { id: ticket.id } 
             expect(response).to redirect_to(dashboard_path)
             end
         end
+        context "as a logged in orginization approved user" do
+            it "redirects to dashboard #tickets:organization" do
+                user = create(:user, :organization_approved)
+                sign_in(user)
+                ticket = create(:ticket, :organization_id => user.organization.id)
+                patch :close, params: { id: ticket.id }
+                expect(response).to redirect_to(dashboard_path << '#tickets:organization') 
+            end
+        end
+        context "as an admin" do
+            it "redirects to dashboard #tickets:open" do
+                user = create(:user, :admin)
+                sign_in(user)
+                ticket = create(:ticket)
+                patch :close, params: { id: ticket.id }
+                expect(response).to redirect_to(dashboard_path << '#tickets:open')
+            end
+        end
+        context "as a logged in user who is not an admin or organization approved" do
+            it "to be successful" do
+                user = create(:user, :organization_approved)
+                sign_in(user)
+                ticket = create(:ticket)
+                patch :close, params: { id: ticket.id }
+                expect(response).to be_successful 
+            end
+        end
+
     end
 
 end

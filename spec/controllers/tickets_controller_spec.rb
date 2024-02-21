d8a600c075b8f8bcaf62a0872b3b13b5d6aa6be9
natitack <114112085+natitack@users.noreply.github.com>
2024-02-21 23:23:36 +0000

    Correct tickets_controller.rb and add example controler spec code

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+# test for close
 RSpec.describe TicketsController, type: :controller do
+    describe "PATCH #close" do
+        context "as a logged out user" do
+            it "redirects to dashboard" do
+            ticket = create(:ticket)
+            patch :close, params: { id: ticket.id }
+            expect(response).to redirect_to(dashboard_path)
+            end
+        end
+    end
 
 end

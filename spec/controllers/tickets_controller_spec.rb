786c0a2928484f5b35ed3e8619fee363cf10c48a
natitack <114112085+natitack@users.noreply.github.com>
2024-02-24 22:15:10 +0000

    Add new and create tests for TicketsController

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,12 +1,61 @@
 require 'rails_helper'
 
-# test for close
+# test for new
+RSpec.describe TicketsController, type: :controller do
+    describe "GET #new" do
+        context "new creates a new ticket" do
+            it "new creates new ticket" do
+                get :new
+                expect(response).to be_successful
+            end
+        end
+    end
+end
+
+#tests for create
+require 'rails_helper'
+
+RSpec.describe TicketsController, type: :controller do
+    require 'rails_helper'
+
+    describe "POST #create" do
+        context "creates a ticket with valid attributes" do
+            it "creates a valid ticket " do
+                ticket = create(:ticket)
+                expect(ticket).to be_valid
+            end
+        end
+        context "saved ticket is redirected to ticket_submitted_path" do
+            it "ticket successfully redirected" do
+                ticket = create(:ticket)
+                expect(ticket.save).to eq(true)
+            end
+        end
+        context "throws an error if ticket is not valid" do
+            it "ticket is not valid" do
+                ticket = build(:ticket, :name => nil)
+                expect(ticket).not_to be_valid
+            end
+        end
+        context "ticket is not saved if it is not valid" do
+            it "ticket is not saved" do
+                ticket = build(:ticket, :name => nil)
+                expect(ticket.save).to eq(false)
+            end
+        end
+    end
+end
+
+
+
+
+# tests for close
 RSpec.describe TicketsController, type: :controller do
     describe "PATCH #close" do
         context "as a logged out user" do
             it "redirects to dashboard" do
             ticket = create(:ticket)
-            patch :close, params: { id: ticket.id } 
+            patch :close, params: { id: ticket.id }
             expect(response).to redirect_to(dashboard_path)
             end
         end
@@ -16,7 +65,7 @@ RSpec.describe TicketsController, type: :controller do
                 sign_in(user)
                 ticket = create(:ticket, :organization_id => user.organization.id)
                 patch :close, params: { id: ticket.id }
-                expect(response).to redirect_to(dashboard_path << '#tickets:organization') 
+                expect(response).to redirect_to(dashboard_path << '#tickets:organization')
             end
         end
         context "as an admin" do
@@ -34,7 +83,7 @@ RSpec.describe TicketsController, type: :controller do
                 sign_in(user)
                 ticket = create(:ticket)
                 patch :close, params: { id: ticket.id }
-                expect(response).to be_successful 
+                expect(response).to be_successful
             end
         end
 

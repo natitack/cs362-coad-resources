6142ef3175278096b6224bec66400b5d0e262bb9
natitack <114112085+natitack@users.noreply.github.com>
2024-02-26 22:48:32 +0000

    Add region and ticket creation feature specs

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
+  before do
+    # create admin
+    admin = create(:user, :admin)
+    # sign in as admin
+    log_in_as(admin)
+  end
+    # navigate to the regions page
+    it 'region can be generated' do
+      click_on 'Regions'
+      click_on 'Add Region'
+      fill_in 'Name', with: 'Test Bend'
+      click_on "Add Region"
 
+
+  # test response
+  expect(page.body).to have_text('Test Bend')
+
+end
 end

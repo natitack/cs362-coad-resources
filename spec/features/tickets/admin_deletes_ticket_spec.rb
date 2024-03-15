c3402a6a608731294ae9bed5f306170c011b8339
kameron beamer <beamerkameron@gmail.com>
2024-03-15 21:51:44 +0000

    Added  end to end delete and capture tests

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
+    before do
+        admin = create(:user, :admin)
+        log_in_as(admin)
+        ticket = create(:ticket, :name => "Test Ticket")
+    end
 
+    it "ticket can be deleted" do
+        click_on "Dashboard"
+        click_on "Test Ticket"
+        click_on "Delete"
+        expect(page).to have_content('was deleted.')
+    end
 end
--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+    before do
+        organization = create(:organization, :name => "Test Organization", :status => "approved")
+        ticket = create(:ticket, :name => "Test Ticket", :organization => organization)
+        user = create(:user, :organization => organization)
+        log_in_as(user)
+    end
 
+    it "ticket can be captured" do
+        click_on "Dashboard"
+        click_on "Tickets"
+        click_on "Test Ticket"
+        expect(page).to have_content("Test Organization")
+    end
 end

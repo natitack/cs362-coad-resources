5b2cd0da02d8e447f6723a39f0829b173cd2cd94
kameron beamer <beamerkameron@gmail.com>
2024-03-15 19:50:08 +0000

    Added application helper tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
+    # Tests for full_title
+    describe "#full_title" do
+        it "returns default title" do
+            expect(helper.full_title).to eq("Disaster Resource Network")
+        end
+
+        it "returns modified param title" do
+            expect(helper.full_title("Test Title")).to eq("Test Title | Disaster Resource Network")
+        end
+    end
 
 end
